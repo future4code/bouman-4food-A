2b191c5e0d7321c5a4894fe04473b41c1968b933
Yan Sabino <yan.sabino@hotmail.com>
2020-02-03 17:36:27 -0300

    inicio da tela de signup

--- src/containers/SignUpPage/index.js
+++ src/containers/SignUpPage/index.js
@@ -4,6 +4,10 @@ import { push } from "connected-react-router";
 import { routes } from "../Router/";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import FourFood from "../../images/logo-future-eats-invert.svg"
+import {StyledImg} from "../../style/styled"
+import AppBarComponent from '../../components/AppBar/AppBar'
+
 
 class SignUpPage extends Component {
   constructor(props) {
@@ -14,7 +18,8 @@ class SignUpPage extends Component {
   render() {
     return (
       <div>
-        <h1> SignUp Page</h1>
+        <AppBarComponent onClickButton={this.props.goToLoginPage}></AppBarComponent>
+        <StyledImg src={FourFood} alt="Logo" />
         <Button onClick={this.props.goToSignUpAddress}>Va para SignUpAddress</Button>
         <Button onClick={this.props.goToLoginPage}>volta para Login</Button>
       </div>
