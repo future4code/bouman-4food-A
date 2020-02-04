a6af6609567b2c1e75fcabc0c861cba605da6519
unknown <lorenzono47@gmail.com>
2020-02-04 14:19:44 -0300

    OrderCard e SignUpAddress

--- src/containers/SignupPageAddress/index.js
+++ src/containers/SignupPageAddress/index.js
@@ -27,15 +27,15 @@ class SignUpPageAddress extends Component {
   render() {
     return (
       <div>
-        <FormTitle>Meu endereço</FormTitle>
         <FormSection>
-          <FormInput classes={{root: 'form'}} name="logradouro" label="Logradouro*" placeholder="Rua / Av." variant="outlined" value={this.state.form.logradouro} onChange={this.handleInputChange} />
-          <FormInput classes={{root: 'form'}} name="numero" label="Número*" placeholder="Número" variant="outlined" value={this.state.form.numero} onChange={this.handleInputChange} />
-          <FormInput classes={{root: 'form'}} name="complemento" label="Complemento" placeholder="Apto. / Bloco" variant="outlined" value={this.state.form.complemento} onChange={this.handleInputChange} />
-          <FormInput classes={{root: 'form'}} name="bairro" label="Bairro*" placeholder="Bairro" variant="outlined" value={this.state.form.bairro} onChange={this.handleInputChange} />
-          <FormInput classes={{root: 'form'}} name="cidade" label="Cidade*" placeholder="Cidade" variant="outlined" value={this.state.form.cidade} onChange={this.handleInputChange} />
-          <FormInput classes={{root: 'form'}} name="estado" label="Estado*" placeholder="Estado" variant="outlined" value={this.state.form.estado} onChange={this.handleInputChange} />
-          <FormButton classes={{root: 'form'}} variant="contained" color="primary">Salvar</FormButton>
+          <FormTitle>Meu endereço</FormTitle>
+          <FormInput classes={{ root: 'form' }} name="logradouro" label="Logradouro*" placeholder="Rua / Av." variant="outlined" value={this.state.form.logradouro} onChange={this.handleInputChange} />
+          <FormInput classes={{ root: 'form' }} name="numero" label="Número*" placeholder="Número" variant="outlined" value={this.state.form.numero} onChange={this.handleInputChange} />
+          <FormInput classes={{ root: 'form' }} name="complemento" label="Complemento" placeholder="Apto. / Bloco" variant="outlined" value={this.state.form.complemento} onChange={this.handleInputChange} />
+          <FormInput classes={{ root: 'form' }} name="bairro" label="Bairro*" placeholder="Bairro" variant="outlined" value={this.state.form.bairro} onChange={this.handleInputChange} />
+          <FormInput classes={{ root: 'form' }} name="cidade" label="Cidade*" placeholder="Cidade" variant="outlined" value={this.state.form.cidade} onChange={this.handleInputChange} />
+          <FormInput classes={{ root: 'form' }} name="estado" label="Estado*" placeholder="Estado" variant="outlined" value={this.state.form.estado} onChange={this.handleInputChange} />
+          <FormButton classes={{ root: 'form' }} variant="contained" color="primary">Salvar</FormButton>
         </FormSection>
       </div>
     );
