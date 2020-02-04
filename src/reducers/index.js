550aaa8f76565490ad541e16398098b9e23de57d
unknown <eduardomendesnorberto@outlook.com>
2020-02-04 16:04:37 -0300

    Ação de login criada

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 
+
 export const generateReducers = history =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+  
     // Outros reducers aqui
   });
